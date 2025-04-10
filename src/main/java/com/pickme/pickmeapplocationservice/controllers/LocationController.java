2c7915baf790fd0811b379d55fcdc5991f7192fa
Rajat Pathak <rajatpathak9888@gmail.com>
2025-04-10 22:13:36 +0530

    adding final changes

--- src/main/java/com/pickme/pickmeapplocationservice/controllers/LocationController.java
+++ src/main/java/com/pickme/pickmeapplocationservice/controllers/LocationController.java
@@ -7,16 +7,13 @@ import com.pickme.pickmeapplocationservice.service.LocationService;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/location")
+@RequestMapping("/api/v1/location")
 public class LocationController {
 
     private LocationService locationService;
@@ -49,5 +46,10 @@ public class LocationController {
 
         }
 
+    }
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "hello";
     }
 }
